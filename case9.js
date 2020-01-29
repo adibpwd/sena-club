7e4bde62ee02a2a33dbf9b81c67a9516f02aeb5d
muslikhuladib0 <0adibcs0@github.io>
2020-01-29 13:31:10 +0700

    nyelesain case9

--- case9.js
+++ case9.js
@@ -1,58 +1,70 @@
 let a = []
 let points = 0;
 let as = []
+let sa = []
+console.log(typeof(sa));
 
 document.getElementById('points').innerHTML = 'point : ' + 0;
+document.getElementById('alert-point').innerHTML = 'selamat bermain';
 
 button = () => {
-    let b = Math.floor(Math.random() * 2);
-    a.push(b)
-    let c = a.length;
     let getId = event.target.id;
     as.push(getId);
+    console.log(getId + "......." + as[0] + '.........' + as);
     
-    if(c == 2) {
-        if(a[0] == a[1]) {
-            points++
-            document.getElementById('points').innerHTML = 'point : ' + points;
-            document.getElementById('alert-point').innerHTML = 'pointmu bertambah 1';
-            document.getElementById(as[0]).removeEventListener('click', bebas);
-            document.getElementById(as[1]).removeEventListener('click', button);
-            console.log(as);
-            
-            // document.getElementById(as[0]).removeAttribute('id');
-            // document.getElementById(as[1]).removeAttribute('id');
-            setTimeout(function() {del1()}, 100)
-        }
+    
+    
+    
+    if(as[0] != getId || as.length == 1) {
+        let b = Math.floor(Math.random() * 2);
+        a.push(b);
+        let c = a.length;
+       
         
-        if(a[0] != a[1]) {
-            document.getElementById('alert-point').innerHTML = 'tidak sama, tidak dapat point.';
-            setTimeout(function() {del()}, 100)
+    
+        document.getElementById(getId).innerHTML = b;
+        if(c == 2) {
+            if(a[0] == a[1]) {
+                points++
+                document.getElementById('points').innerHTML = 'point : ' + points;
+                document.getElementById('alert-point').innerHTML = 'pointmu bertambah 1';
+                document.getElementById(as[0]).removeAttribute('onclick');
+                document.getElementById(as[1]).removeAttribute('onclick');
+                console.log(as);
+                document.getElementById(as[0]).removeAttribute('id');
+                document.getElementById(as[1]).removeAttribute('id');
+                setTimeout(delTrue(), 1000)
+            }
+            
+            if(a[0] != a[1]) {
+                document.getElementById('alert-point').innerHTML = 'tidak sama, tidak dapat point.';
+                setTimeout(refresh, 1000)
+                setTimeout(delFalse, 2000)
+                
+            }
         }
+
+    }  else {
+        as.splice(0, 1)
+          alert ('klik yang lain..ini sudah kamu klik');
+        
     }
-    document.getElementById(getId).innerHTML = b;
+   
+
 }
 
-del = () => {
+delTrue = () => {
     a.splice(0, 2);
     as.splice(0, 2);
 }
 
-del1 = () => {    
+delFalse = () => {
+   
     a.splice(0, 2);
-    
+    as.splice(0, 2);
 }
 
-jajal = () => {
-    document.getElementsByTagName('p')[5].setAttribute('class', 'jok')
-}
-
-as1 = () => {
-     let aj = event.target.id;
-     document.getElementById(aj).innerHTML = 'jjjj'
-}
-
-bebas = () => {
-    console.log('mantap');
-    
+refresh = () => {
+    document.getElementById(as[0]).innerHTML = '?';
+    document.getElementById(as[1]).innerHTML = '?';
 }
